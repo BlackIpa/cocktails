34cb7bc2142d1aeab5260799376aa64320812578
BlackIpa <augustyniak.p@Gmail.com>
2023-05-30 19:34:15 +0200

    Added additional fields to Customer and created CORs configuration to allow frontend to connect. For now, moved back to local db

--- src/main/java/com/cocktails/config/SecurityConfig.java
+++ src/main/java/com/cocktails/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cocktails.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -14,8 +19,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }).and().csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/cocktails/1").authenticated()
                 .requestMatchers("/cocktails/**", "/register").permitAll()
--- src/main/java/com/cocktails/user/model/Customer.java
+++ src/main/java/com/cocktails/user/model/Customer.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
@@ -24,10 +26,18 @@ public class Customer {
     @Column(name = "email")
     private String email;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     @Column(name = "pwd")
     private String pwd;
 
     @Column(name = "role")
     private String role;
 
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
